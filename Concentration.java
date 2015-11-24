4c7d422204ded148e587cf6427ab1266c04ce2ab
Chris <chris@autumn.(none)>
2015-11-24 00:35:38 -0500

    Complete

--- Concentration.java
+++ Concentration.java
@@ -1,3 +1,8 @@
+/*  Team GitRektAgain - Sachal Malick, Vincent Alykin, Christopher Sherling
+    APCS1 pd5
+    HW36 -- Some Folks Call It a Memory
+    2015-11-23 */
+
 import java.util.Scanner;
 
 //import cs1.Keyboard;  //if comfortable with Scanner, you may comment this out
@@ -10,7 +15,8 @@ public class Concentration {
     //instance variables
     private Tile[][] _board;     //storage for 4x4 grid of Tiles.
     private int _numberFaceUp;   //count of Tiles with faces visible
-    private String[] _words = {"apple","bob","casey","dog","elephant","frog","hello","igloo","jaco","chondsalb","yocanna","dcsoo","choov","oopuy","hof","uiop"};;     //list of unique Strings used for Tile vals
+    private String[] _words = {"art", "art", "bob", "bob", "cat", "cat","dog","dog","eel","eel","fin","fin","get","get","ice","ice"};     //list of unique Strings used for Tile vals
+    //private String[] _words = {"art","bob","cat","dog","eel","fin","get","ice"};
     private static int _numRows = 4;
 
 
@@ -29,7 +35,7 @@ public class Concentration {
     		
     	    System.out.println();
     	} 
-        }
+    }
 
     private void swap( int i, int j ) {
 	String extra = _words[i];
@@ -45,91 +51,95 @@ public class Concentration {
 		
     public void populate(){
 	randomize();
-	for(int j=0; j< 2; j++){ //does the first two rows
-	    for(int k=0; k < this._board[j].length; k++){ //does the rows
-		this._board[j][k] = new Tile (_words[j * k + 4]);
-	    }	
-	}
-	randomize();
-	for(int x=2; x< 4; x++){ //does the second two rows
-	    for(int y=0; y < this._board[x].length; y++){ //does the rows
-		this._board[x][y] = new Tile (_words[x * y + 4]);
-	    }	
+	// for(int j=0; j< 2; j++){ //does the first two rows
+	//     for(int k=0; k < this._board[j].length; k++){ //does the rows
+	// 	this._board[j][k] = new Tile (_words[j * 4 + k]);
+	//     }	
+	// }
+	// randomize();
+	// for(int x=2; x< 4; x++){ //does the second two rows
+	//     for(int y=0; y < this._board[x].length; y++){ //does the rows
+	// 	this._board[x][y] = new Tile (_words[(x-2) * 4 + y]);
+	//     }	
+	// }
+	for(int i=0; i<_board.length; i++){
+	    for(int j=0; j<_board[i].length; j++)
+	 	this._board[i][j] = new Tile (_words[i * 4 + j]);	    
 	}
     }
     
-    public String play() {
+    public void play() {
     	populate();
     	while (_numberFaceUp < 16) {
     		
     	
-    	this.print2();
-    	Scanner s = new Scanner(System.in);
-    	String firstcol = "";
-    	String firstrow = "";
-    	String seccol = "";
-    	String secrow = "";
+	    this.print2();
+	    Scanner s = new Scanner(System.in);
+	    String firstcol = "";
+	    String firstrow = "";
+	    String seccol = "";
+	    String secrow = "";
 
     	
-    	System.out.println("Please enter the row of the first string");
+	    System.out.println("Please enter the row of the first string");
 
-    	if (s.hasNext() ) {
-    	firstrow = s.nextLine();
+	    if (s.hasNext() ) {
+		firstrow = s.nextLine();
 
-    	}
+	    }
 
-    	System.out.println("Please enter the column of the first string");
+	    System.out.println("Please enter the column of the first string");
 
-    	if (s.hasNext() ) {
+	    if (s.hasNext() ) {
 
-    	firstcol = s.nextLine();
+		firstcol = s.nextLine();
 
-    	}
-    	System.out.println("Please enter the row of the second string");
+	    }
+	    System.out.println("Please enter the row of the second string");
 
-    	if (s.hasNext() ) {
+	    if (s.hasNext() ) {
 
-    	secrow = s.nextLine();
+		secrow = s.nextLine();
 
-    	}
-    	System.out.println("Please enter the column of the second string");
+	    }
+	    System.out.println("Please enter the column of the second string");
 
-    	if (s.hasNext() ) {
+	    if (s.hasNext() ) {
 
-    	seccol = s.nextLine();
+		seccol = s.nextLine();
 
-    	}
+	    }
 
 
-    	int firsrow = Integer.parseInt(firstrow);
-    	int secundorow = Integer.parseInt(secrow);
-    	int firstcolo = Integer.parseInt(firstcol);
-    	int secundocolo = Integer.parseInt(seccol);
-    	Tile t = this._board[firsrow][firstcolo];
-    	Tile j = this._board[secundocolo][secundorow];
+	    int firsrow = Integer.parseInt(firstrow);
+	    int secundorow = Integer.parseInt(secrow);
+	    int firstcolo = Integer.parseInt(firstcol);
+	    int secundocolo = Integer.parseInt(seccol);
+	    Tile t = this._board[firsrow][firstcolo];
+	    Tile j = this._board[secundorow][secundocolo];
     	
-    	t.set_isFaceUp(true);
-    	j.set_isFaceUp(true);
+	    t.set_isFaceUp(true);
+	    j.set_isFaceUp(true);
 
-    	this.print2();
-    	if (!t.equals(j)) {
+	    this.print2();
+	    if (!t.equals(j)) {
     		t.set_isFaceUp(false);
         	j.set_isFaceUp(false);
-    	}
-    	else {
+	    }
+	    else {
     		_numberFaceUp+=2;
-    	}
-    	this.print2();
+	    }
+	    System.out.println();
     	}
     	
-    	return "game over";
+    	System.out.println("game over");
     }
 		
-		//DO NOT MODIFY main()
-		public static void main(String[] args){
-				Concentration game = new Concentration();
-				game.play();
-		}
+    //DO NOT MODIFY main()
+    public static void main(String[] args){
+	Concentration game = new Concentration();
+	game.play();
+    }
 
 }//end class Concentration
 
