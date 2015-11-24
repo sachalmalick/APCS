94b4c8136171b9fa320e4408bc0a2b1a0245e6e4
Chris <chris@autumn.(none)>
2015-11-23 22:17:31 -0500

    Disaster

--- Concentration.java
+++ Concentration.java
@@ -11,12 +11,39 @@ public class Concentration {
 
 
     //insert constructor and methods here
+    public Concentration () {
+	_board = new int [4][4];
+	_numberFaceUp = 0;
+	_words = {"apple","bob","casey","dog","elephant","frog","hello","igloo"};
+    }
 
-    // public Concentration(){
-    // 	Tile[][] = 
-    // }
+    private void swap( int i, int j ) {
+	String extra = _words[i];
+	_words[i] = _words[j];
+	_words[j] = extra;
+    }
+	
+    public void randomize() {
+	for (int i=0; i< _words.length;i++){
+	    swap(i, (int)(Math.random()*(_words.length)));
+	}
+    }
+	
+    public void populate(){
+	randomize();
+	for(int j=0; j< 2; j++){ //does the first two rows
+	    for(int k=0; k < a[j].length; k++){ //does the rows
+		a[j][k] = _words[j * k + 4];
+	    }	
+	}
+	randomize();
+	for(int x=2; x< 4; x++){ //does the second two rows
+	    for(int y=0; y < a[x].length; y++){ //does the rows
+		a[x][y] = _words[x * y + 4];
+	    }	
+	}
+    }
 
-		
     //DO NOT MODIFY main()
     public static void main(String[] args){
 	Concentration game = new Concentration();
